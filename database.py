f0ae091c0b9f9ae372d7cb57b60dfc80efd5106f
Ali alipour <alipourkabeer99@gmail.com>
2025-07-27 18:46:24 +0330

    connecting to mysql

--- database.py
+++ database.py
@@ -5,10 +5,11 @@ import os
 
 # SQLite connection string for local debugging (no Docker)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'products_db.sqlite3')}"
+# MySQL connection string
+DATABASE_URL = "mysql+pymysql://root:12345678@localhost:3306/products_db"
 
 # Create SQLAlchemy engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 # Create SessionLocal class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
